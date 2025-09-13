fc9c2c7ddf1b5fd0c81a446931af0b371655447e
Dmitriy Tkachenko <dtkachenko4@alfabank.ru>
2025-09-13 15:38:46 +0300

    add

--- src/main/java/com/example/hibernatelesson/repository/jpa/JpaAuthorRepository.java
+++ src/main/java/com/example/hibernatelesson/repository/jpa/JpaAuthorRepository.java
@@ -3,7 +3,6 @@ package com.example.hibernatelesson.repository.jpa;
 import com.example.hibernatelesson.models.DbBeanType;
 import com.example.hibernatelesson.models.hibernate.AuthorEntity;
 import com.example.hibernatelesson.repository.AuthorRepository;
-import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
